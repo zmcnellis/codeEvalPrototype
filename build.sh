d176f9df8ea6a918b92d780780a8b8cd8f148f8c
Zachary McNellis <zmcnell@g.clemson.edu>
2016-01-26 15:30:22 -0500

    changed to clang 3.4

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 set -ue
 
-curl -qO http://llvm.org/releases/3.2/llvm-3.2.src.tar.gz
-curl -qO http://llvm.org/releases/3.2/clang-3.2.src.tar.gz
-curl -qO http://llvm.org/releases/3.2/compiler-rt-3.2.src.tar.gz
+curl -qO http://llvm.org/releases/3.4/llvm-3.4.src.tar.gz
+curl -qO http://llvm.org/releases/3.4/clang-3.4.src.tar.gz
+curl -qO http://llvm.org/releases/3.4/compiler-rt-3.4.src.tar.gz
 
-tar -zxf llvm-3.2.src.tar.gz
-tar -zxf clang-3.2.src.tar.gz
-tar -zxf compiler-rt-3.2.src.tar.gz
+tar -zxf llvm-3.4.src.tar.gz
+tar -zxf clang-3.4.src.tar.gz
+tar -zxf compiler-rt-3.4.src.tar.gz
 
-mv clang-3.2.src llvm-3.2.src/tools/clang
-mv compiler-rt-3.2.src llvm-3.2.src/projects/compiler-rt
+mv clang-3.4.src llvm-3.4.src/tools/clang
+mv compiler-rt-3.4.src llvm-3.4.src/projects/compiler-rt
 
 mkdir build
 cd build
-../llvm-3.2.src/configure --prefix=/app/vendor --disable-docs
+../llvm-3.4.src/configure --prefix=/app/vendor --disable-docs
 make -sj4
 make install
